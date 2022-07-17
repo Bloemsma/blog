7f78928b9bdbf821090288127bedf4ce087661ba
Boyd Bloemsma <info@boydbloemsma.com>
2022-07-17 21:09:43 +0200

    Added SEO and changed favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,12 @@ const Layout: FC<LayoutProps> = ({ children, title = 'Massaal blog' }) => (
                 name="viewport"
                 content="initial-scale=1.0, width=device-width"
             />
+            <meta name="robots" content="all" />
+            <meta
+                name="description"
+                content="Web development blog for Massaal"
+            />
+            <link rel="icon" type="image/svg+xml" href="favicon.svg" />
         </Head>
         <div className="text-center pt-3 mb-8 md:pb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
             Massaal
